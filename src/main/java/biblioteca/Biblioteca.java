61a70ba2a8bbb4f709e1e8c7df80f59f7658b5d2
Alberto Balaguer <alberto.balaguer184@comunidadunir.netgit config --global user.emailgit config --global user.email alberto.balaguer184@comunidadunir.net>
2025-03-02 19:30:39 +0100

    DOC: Creados los comentarios de JavaDoc de la clase Biblioteca.

--- src/main/java/biblioteca/Biblioteca.java
+++ src/main/java/biblioteca/Biblioteca.java
@@ -4,24 +4,50 @@ package biblioteca;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa una biblioteca con una colección de libros.
+ * Permite agregar, eliminar y buscar libros pro título y autor.
+ *
+ * @see Libro (clase)
+ * @author Alberto
+ * @version 4.3.2025
+ */
+
 public class Biblioteca {
 
     // TODO: Documentar estos atributos
+    /** Lista de libros disponibles en la biblioteca*/
     private final List<Libro> libros;
 
     // TODO: Documentar este método
+    /** Constructor por defecto de la biblioteca sin libros*/
     public Biblioteca() {
         libros = new ArrayList<>();
     }
 
     // TODO: Documentar este método.
     //  Test: NO HAY QUE TESTEAR ESTE METODO
+
+    /**
+     * Constructor con parámetros
+     * @param libros lista de libros que inicializan
+     */
+
     public Biblioteca(List<Libro> libros) {
         this.libros = libros;
     }
 
     // TODO: Testear este metodo.
     //  Test: Comprobar si se ha agregado
+
+    /**
+     * método booleano para agregar libro a nuestra biblioteca
+     * @param libro Un libro para agregar
+     * @see Libro (clase)
+     * @return {@code true} si el libro se ha agregado correctamente o {@code flase} si el libro no se ha agregado
+     * correctamente.
+     */
+
     public boolean agregarLibro(Libro libro) {
         return libros.add(libro);
     }
@@ -33,6 +59,11 @@ public class Biblioteca {
     }
 
     // TODO: Documentar este método
+
+    /**
+     * Devuelve la lista de libros de la biblioteca.
+     * @return lista de libros de la biblioteca.
+     */
     public List<Libro> getLibros() {
         return libros;
     }
@@ -40,6 +71,13 @@ public class Biblioteca {
     // TODO: Documentar este método.
     //  Test 01: buscar libro existente y comprobar que lo localiza.
     //  Test 02: buscar libro NO existente y comprobar que no lo localiza.
+
+    /**
+     *
+     * @param titulo
+     * @return libro
+     */
+
     public Libro encuentraLibroPorTitulo(String titulo) {
         for (Libro libro : libros) {
             if (libro.getTitulo().equals(titulo)) {
@@ -51,6 +89,14 @@ public class Biblioteca {
 
     // TODO: Documentar este metodo.
     //  No testearlo
+
+    /**
+     * @deprecated Este metodo ha quedado obsoleto.
+     * Se recomienda usar {@link #encuentraLibrosPorAutor(String)} en su lugar.
+     * @param autor
+     * @return libro.
+     */
+    
     // Este metodo está obsoleto. Hay que documentarlo como tal.
     //  Recuerda: las anotaciones @deprecated y @link a la nueva
     //  versión mejorada encuentraLibrosPorAutor(...)
@@ -70,6 +116,14 @@ public class Biblioteca {
     // TODO: Testear este metodo.
     //  Test: Comprobar la lista de libros que devuelve para un autor existentes.
     //  Test: Comprobar la lista de libros que devuelve para un autor No existente
+
+    /**
+     * Busca todos los libros de un autor determinado.
+     * @since v2.0
+     * Sustituye al metodo {@link #encuentraLibrosPorAutor(String)} (String)} en su lugar.
+     * @param autor
+     * @return
+     */
     public List<Libro> encuentraLibrosPorAutor(String autor) {
         List<Libro> listaLibros = null;
         for (Libro libro : libros) {
